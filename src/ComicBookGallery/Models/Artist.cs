0f4a99bf78d125b427a73aa9fb94f9f3052dd5bc
tuey_14 <Tuey_14@hotmail.co.uk>
2018-03-15 23:01:00 +0000

    Updated Comic Book Detail View to be Strongly typed

--- src/ComicBookGallery/Models/Artist.cs
+++ src/ComicBookGallery/Models/Artist.cs
@@ -10,6 +10,6 @@ namespace ComicBookGallery.Models
 
         public string Name { get; set; }
         public string Role { get; set; }
-
+        
     }
 }
--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -31,5 +31,7 @@ namespace ComicBookGallery.Models
             }
             
         }
+        
      }
+    
 }

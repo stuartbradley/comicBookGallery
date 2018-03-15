382787f604f4996e5b7a62de142b274399cbe7cf
tuey_14 <Tuey_14@hotmail.co.uk>
2018-03-15 22:36:01 +0000

    Added comic book and artist models

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -13,14 +13,14 @@ namespace ComicBookGallery.Controllers
         {
 
             ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
+            ViewBag.IssueNumber = 800;
             ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
             ViewBag.Artists = new String[]
             {
                 "Script: Dan Slott",
                 "Pencils: Humberto Ramos",
                 "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
+                "Colors: Amy Jones",
                 "Letters: Chris Eliopoulos"
             };
         
